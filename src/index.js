307e07b88a36d630e3685b9b98b7bfab687c0439
Onufriieva <onufrieva.julia@gmail.com>
2022-08-13 14:14:14 +0300

    Добавила симпллайтбокс

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import './css/styles.css';
 import Notiflix from 'notiflix';
 // import { fetchCountries } from './fetch';
 import axios from "axios";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const refs = {
 input: document.querySelector('input'),
 form: document.querySelector('.search-form'),
 buttonLoad: document.querySelector('.load-more'),
-gallery: document.querySelector('.gallery')
+gallery: document.querySelector('.gallery'),
 }
 
 const BASE_URL = "https://pixabay.com/api/";
@@ -16,6 +18,7 @@ refs.form.addEventListener('submit', (onFormSubmit));
 refs.input.addEventListener('input', () => {});
 
 let nameSearch = refs.input.value;
+let lightbox = null;
  
 
 async function fetchImages() {
@@ -34,9 +37,19 @@ async function fetchImages() {
     }
 }
 
+
+const createLightBox = () => {
+    lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    });
+};
+
+
     
 function onFormSubmit(e) {    
-    e.preventDefault()
+    // e.preventDefault()
 nameSearch = refs.input.value;
     if(nameSearch === "") {
         cleanMarkup()
@@ -47,11 +60,15 @@ nameSearch = refs.input.value;
     .then(images => {
       insertMarkup(images);
     }).catch(error => (console.log(error)))
+
+    createLightBox();
+    lightbox.on('')
+
   }
 
   const createMarkup = img => `
   <div class="photo-card">
-         <a href="${img.largeImageURL}">
+         <a href="${img.largeImageURL} class="gallery_link">
           <img class="gallery__image" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
           </a>
         <div class="info">
@@ -70,7 +87,7 @@ nameSearch = refs.input.value;
         </div>
     </div>
         `;
-
+      
 
   function insertMarkup(arrayImages) {
     const result = generateMarkup(arrayImages);
@@ -86,6 +103,7 @@ nameSearch = refs.input.value;
   function cleanMarkup(){
       refs.gallery.innerHTML = " ";
   }
+
 
 
 onFormSubmit()
